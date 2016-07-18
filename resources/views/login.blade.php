f8002aa1f95b21cea3d9074731273daf584b7ce6
jasonhong <jasonhong0903@gmail.com>
2016-07-18 12:29:58 +0800

    "update"

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,15 +9,15 @@
             <tbody>
             <tr>
                 <td>帳號</td><td><input type="text" name="login" /></td>
-            </tr>
+            </tr>git
             <tr>
                 <td>密碼</td><td><input type="password" name="pass" /></td>
             </tr>
             <tr>
                 <td colspan="2">
-                    <input type="submit" value="送出!" />
-                    <input type="reset" value="取消!" />
-                    <input type="button" value="回首頁!" onclick="window.location='/'"/>
+                    <input type="submit" value="送出" />
+                    <input type="reset" value="取消" />
+                    <input type="button" value="回首頁" onclick="window.location='/'"/>
                 </td>
             </tr>
             </tbody>
