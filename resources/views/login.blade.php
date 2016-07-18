f97852b501188c7ad45c3d032b5db59209bcc571
jasonhong <jasonhong0903@gmail.com>
2016-07-18 13:38:17 +0800

    "asd"

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -15,9 +15,9 @@
             </tr>
             <tr>
                 <td colspan="2">
-                    <input type="submit" value="送出!" />
-                    <input type="reset" value="取消!" />
-                    <input type="button" value="回首頁!" onclick="window.location='/'"/>
+                    <input type="submit" value="送出" />
+                    <input type="reset" value="取消" />
+                    <input type="button" value="回首頁" onclick="window.location='/'"/>
                 </td>
             </tr>
             </tbody>
